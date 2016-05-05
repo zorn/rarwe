83ad97e2e95abbf0d125fe5dac974a50eb2eb229
Michael Zornek <mike@clickablebliss.com>
2016-05-04 23:36:10 -0400

    End of chapter 3

---
+++ app/routes/application.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+})
+
+
+export default Ember.Route.extend({
+  model: function() {
+    var blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+    var yellowLedbetter = Song.create({
+      title: 'Yellow Lednbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+    var pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+    return [blackDog, yellowLedbetter, pretender];
+  }
+});
