6bb875a6b93eb9ce28b18b6c7031fd4b2831016d
Michael Zornek <mike@clickablebliss.com>
2016-05-17 18:20:04 -0400

    End of chapter 7.

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+    updateRating: function(params) {
+      var song = params.item,
+      rating = params.rating;
+
+      song.set('rating', rating);
+    },
+  }
+
+});
